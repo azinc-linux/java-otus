d1eb56ec95c58517d042a786c35957bf3b152d58
azinc <azinc@yandex.ru>
2019-10-09 12:00:47 +0300

    correct pom.xml;  add package otusjava;

---
+++ hw01-maven/src/main/java/otusjava/HelloOtus.java
@@ -0,0 +1,22 @@
+package otusjava;
+
+import com.google.common.collect.Lists;
+import java.util.List;
+
+public class HelloOtus {
+
+    private static List<String> getArrFromString(String pattern){
+        List<String> strlist = Lists.newArrayList();
+        String[] strarray = pattern.split(" ");
+        for(String i : strarray)
+            strlist.add(i);
+        return strlist;
+    }
+ public static void main(String[] args) {
+        List<String> strlist = getArrFromString("aaa bbb ccc ddd eee fff");
+        for (String i : strlist)
+            System.out.println(i);
+
+ }
+
+}
